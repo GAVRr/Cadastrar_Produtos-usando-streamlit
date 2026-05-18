a5bf9cdc9cf5a70bab5f0e9ec6d3624445fe769e
Gabriel vitor reuter <104223902+GAVRr@users.noreply.github.com>
2026-05-17 23:12:52 -0300

    Delete pyautogui directory

--- pyautogui/pegar_posicao.py
+++
@@ -1,6 +0,0 @@
-import pyautogui as py
-# from time import sleep
-# #pegar posições do mouse e da tela
-# sleep(5)
-# print(pyautogui.position())
-py.displayMousePosition()
--- pyautogui/teste.py
+++
@@ -1,8 +0,0 @@
-from keyboard import is_pressed
-
-while True:
-    if is_pressed("w"):
-        print("Andando")
-
-    if not is_pressed('w'):
-        print('parado')
